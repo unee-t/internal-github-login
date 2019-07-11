7567deeeecf0d3afaa4b0f2594d523638671958b
Kai Hendry <kai.hendry@unee-t.com>
2019-07-11 16:30:19 +0800

    Make a package that can be used from other projects

--- README.md
+++ README.md
@@ -3,8 +3,19 @@ organisation before setting secure cookie.
 
 <img src="https://media.dev.unee-t.com/2019-02-18/internal-app.gif">
 
-Example from
-https://github.com/dghubble/gologin/blob/master/examples/github/main.go with
-modifications.
-
 https://github.com/organizations/unee-t/settings/applications/978656
+
+# How to use
+
+	import login "github.com/unee-t/internal-github-login"
+
+	func BasicEngine() http.Handler {
+		adminHandlers := alice.New(login.RequireUneeT)
+		app := login.GithubOrgOnly() // sets up special routes like GH callback
+		app.Handle("/", adminHandlers.ThenFunc(indexHandler))
+		return app
+	}
+
+	func indexHandler(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "For Unee-T eyes only")
+	}
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
-package main
+package login
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"os"
 	"text/template"
 
-	"github.com/apex/log"
-	jsonhandler "github.com/apex/log/handlers/json"
 	"github.com/dghubble/gologin"
 	"github.com/dghubble/gologin/github"
+
+	"github.com/apex/log"
 	gogithub "github.com/google/go-github/github"
 	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
@@ -38,12 +37,11 @@ const (
 
 // sessionStore encodes and decodes session data stored in signed cookies
 var store = sessions.NewCookieStore([]byte(sessionSecret), nil)
+
+// Try https://github.com/gobuffalo/packr ?
 var views = template.Must(template.ParseGlob("templates/*.html"))
 
-func init() {
-	log.SetHandler(jsonhandler.Default)
-}
-
+// Refactor to use context instead probably
 func routeLog(r *http.Request) *log.Entry {
 	l := log.WithFields(log.Fields{
 		"id": r.Header.Get("X-Request-Id"),
@@ -52,26 +50,23 @@ func routeLog(r *http.Request) *log.Entry {
 	return l
 }
 
-// New returns a new ServeMux with app routes.
-func New() *http.ServeMux {
+// GithubOrgOnly returns a new ServeMux with app routes.
+func GithubOrgOnly() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// TODO: Ideally want to say that any path redirects to a /login if unauthenticated
-	// i.e. skip the weclomeHandler
+	mux.Handle("/profile", RequireUneeT(http.HandlerFunc(profileHandler)))
 
-	mux.HandleFunc("/", welcomeHandler)
-	mux.Handle("/profile", requireLogin(http.HandlerFunc(profileHandler)))
 	mux.HandleFunc("/logout", logoutHandler)
-	mux.HandleFunc("/not-part-of-org", notPartOfOrg)
 	// 1. Register LoginHandler and CallbackHandler
 	oauth2Config := &oauth2.Config{
 		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
 		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
-		RedirectURL:  fmt.Sprintf("https://%s/github/callback", os.Getenv("DOMAIN")),
-		Endpoint:     githubOAuth2.Endpoint,
+		// RedirectURL:  fmt.Sprintf("https://%s/github/callback", os.Getenv("DOMAIN")),
+		RedirectURL: "http://localhost:3000/github/callback",
+		Endpoint:    githubOAuth2.Endpoint,
 	}
 
-	stateConfig := gologin.DefaultCookieConfig
+	stateConfig := gologin.DebugOnlyCookieConfig
 	mux.Handle("/github/login", github.StateHandler(stateConfig, github.LoginHandler(oauth2Config, nil)))
 	mux.Handle("/github/callback", github.StateHandler(stateConfig, github.CallbackHandler(oauth2Config, issueSession(), nil)))
 	return mux
@@ -97,7 +92,7 @@ func issueSession() http.Handler {
 		}
 
 		if !member {
-			http.Redirect(w, req, "/not-part-of-org", http.StatusFound)
+			views.ExecuteTemplate(w, "notPartOfOrg.html", wanted)
 			return
 		}
 		log.WithField("user", githubUser.GetName()).Info("confirmed member")
@@ -116,7 +111,7 @@ func issueSession() http.Handler {
 			return
 		}
 
-		http.Redirect(w, req, "/profile", http.StatusFound)
+		http.Redirect(w, req, "/", http.StatusFound)
 	}
 	return http.HandlerFunc(fn)
 }
@@ -146,26 +141,6 @@ func isPartOfOrg(githubUser *gogithub.User, Org GithubOrg) (member bool, err err
 	return
 }
 
-func notPartOfOrg(w http.ResponseWriter, req *http.Request) {
-	views.ExecuteTemplate(w, "notPartOfOrg.html", wanted)
-}
-
-// welcomeHandler shows a welcome message and login button.
-func welcomeHandler(w http.ResponseWriter, req *http.Request) {
-	log := routeLog(req)
-	if req.URL.Path != "/" {
-		http.NotFound(w, req)
-		return
-	}
-	if isAuthenticated(req) {
-		log.Info("authenticated")
-		http.Redirect(w, req, "/profile", http.StatusFound)
-		return
-	}
-	log.Warn("unauthenticated")
-	views.ExecuteTemplate(w, "home.html", nil)
-}
-
 // profileHandler shows protected user content.
 func profileHandler(w http.ResponseWriter, req *http.Request) {
 	log := routeLog(req)
@@ -179,33 +154,31 @@ func profileHandler(w http.ResponseWriter, req *http.Request) {
 
 }
 
-// logoutHandler destroys the session on POSTs and redirects to home.
+// logoutHandler destroys the session
 func logoutHandler(w http.ResponseWriter, req *http.Request) {
 	log := routeLog(req)
-	if req.Method == "POST" {
-		session, err := store.Get(req, sessionName)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		session.Options.MaxAge = -1
-		err = session.Save(req, w)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		log.Info("deleting cookie")
+	session, err := store.Get(req, sessionName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	session.Options.MaxAge = -1
+	err = session.Save(req, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Info("deleting cookie")
 	http.Redirect(w, req, "/", http.StatusFound)
 }
 
-// TODO: Move into middleware?
-// requireLogin redirects unauthenticated users to the login route.
-func requireLogin(next http.Handler) http.Handler {
+// RequireUneeT redirects unauthenticated users to the login route.
+func RequireUneeT(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, req *http.Request) {
 		if !isAuthenticated(req) {
-			http.Redirect(w, req, "/", http.StatusFound)
+			log.Warn("unauthenticated")
+			views.ExecuteTemplate(w, "unauthenticated.html", nil)
 			return
 		}
 		next.ServeHTTP(w, req)
@@ -213,7 +186,6 @@ func requireLogin(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
-// Important NOT TO SCREW THIS UP
 // isAuthenticated returns true if the user has a signed session cookie.
 func isAuthenticated(req *http.Request) bool {
 	log := routeLog(req)
@@ -229,13 +201,4 @@ func isAuthenticated(req *http.Request) bool {
 	// Is user id is set, we consider the person as logged in!?
 	_, ok := session.Values[sessionUserKey]
 	return ok
-}
-
-// main creates and starts a Server listening.
-func main() {
-	// read credentials from environment variables if available
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), New())
-	if err != nil {
-		log.WithError(err).Fatal("error listening")
-	}
 }
